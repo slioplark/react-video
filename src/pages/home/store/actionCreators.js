bbac20a9af642799ffe13c226b82a8fba3bb89bf
Mason <jeailfy9520@outlook.com>
2021-03-03 20:47:07 +0800

    feat: add error message

--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -15,7 +15,7 @@ export const getList = (pageToken = '') => {
   return (dispatch) => {
     axios.get(`${api}?part=${part}&chart=${chart}&maxResults=100&key=${key}&pageToken=${pageToken}`)
       .then(res => dispatch(chageList(res.data)))
-      .catch(err => console.log(err));
+      .catch(err => alert('由於多次得調用 youtube api，所以目前暫時無法回傳資料，請耐心等待一段時間後，再重新進入頁面。'));
   }
 }
 
