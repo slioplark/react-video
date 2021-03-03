b3ec399e31d6e71777410cf8a5894d83fbc2eac4
Mason <jeailfy9520@outlook.com>
2021-03-03 10:38:27 +0800

    feat: ad

--- src/pages/play/index.js
+++ src/pages/play/index.js
@@ -1,39 +1,72 @@
-import { useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { actionCreators } from './store';
-import { PlayWrapper, Title, Desc, Divider } from './style';
+import { PlayWrapper, Ad, Title, Desc, Divider } from './style';
 import videojs from 'video.js';
 
 function Play(props) {
+  const [isAd, setIsAd] = useState(false);
   const data = useSelector(state => state.play.data);
   const dispatch = useDispatch();
   const playerRef = useRef();
 
-  const setPlayer = () => {
-    const player = videojs(
-      playerRef.current,
-      { autoplay: true, muted: true },
-      () => {
-        player.src(
-          'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
-        );
-      }
-    );
+  const playAd = () => {
+    setIsAd(true);
+  }
+
+  const stopAd = () => {
+    setIsAd(false);
+  }
+
+  const skipAd = () => {
+    setIsAd(false);
+    playerRef.current.play();
   }
 
   useEffect(() => {
     const id = props.match.params.id;
     dispatch(actionCreators.getData(id));
-    setPlayer();
 
     return () => {
       dispatch(actionCreators.clearData());
     }
   }, [props, dispatch]);
 
+  useEffect(() => {
+    const player = videojs(
+      playerRef.current,
+      {
+        muted: true,
+        autoplay: true
+      },
+      () => {
+        player.src(
+          'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
+        );
+      }
+    );
+  }, []);
+
   return (
     <PlayWrapper>
-      <video className="video-js" controls ref={playerRef}></video>
+      <video
+        controls
+        className="video-js"
+        ref={playerRef}
+        onPlay={stopAd}
+        onPause={playAd}
+      >
+      </video>
+      {
+        isAd
+          ? (
+            <Ad>
+              <span>AD Time</span>
+              <button onClick={skipAd}>Skip</button>
+            </Ad>
+          )
+          : ''
+      }
       <Title>{data?.snippet?.channelTitle}</Title>
       <Divider />
       <Desc dangerouslySetInnerHTML={{ __html: data?.snippet?.description }} />
--- src/pages/play/style.js
+++ src/pages/play/style.js
@@ -1,16 +1,43 @@
 import styled from 'styled-components';
 
 export const PlayWrapper = styled.div`
+  position: relative;
   width: 100%;
   max-width: 1080px;
   margin: 16px auto;
+  padding: 0 4px;
   box-sizing: border-box;
   img {
     width: 100%;
   }
   .video-js {
     width: 100%;
-    padding-bottom: 50%;
+    height: 540px;
+    @media only screen and (max-width: 420px) {
+      height: 250px;
+    }
+  }
+`;
+
+export const Ad = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 1080px;
+  height: 540px;
+  color: #fff;
+  background-color: #000;
+  font-size: 24px;
+  @media only screen and (max-width: 420px) {
+    height: 250px;
+  }
+  button {
+    margin: 10px;
+    padding: 10px;
   }
 `;
 
