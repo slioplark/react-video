f3152a27bd174cf3945d7bd3b468f0e88b81e160
Mason <jeailfy9520@outlook.com>
2021-04-11 15:53:02 +0800

    feat: install react-hook-form

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,24 +1,41 @@
+import { useForm } from 'react-hook-form';
 import { FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
-import { LoginWrapper } from './style'
+import { LoginWrapper } from './style';
 
 function Login() {
+  const { handleSubmit, register } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values);
+  }
+
   return (
     <LoginWrapper>
-      <FormControl id="account" isRequired>
-        <FormLabel>Account</FormLabel>
-        <Input placeholder="Account" />
-      </FormControl>
-      <FormControl id="password" isRequired>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" placeholder="Password" />
-      </FormControl>
-      <Button
-        mt={2}
-        type="submit"
-        colorScheme="teal"
-      >
-        Submit
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isRequired>
+          <FormLabel>Account</FormLabel>
+          <Input
+            placeholder="Account"
+            {...register('account')}
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            placeholder="Password"
+            autoComplete="off"
+            {...register('password')}
+          />
+        </FormControl>
+        <Button
+          mt={2}
+          type="submit"
+          colorScheme="teal"
+        >
+          Submit
       </Button>
+      </form>
     </LoginWrapper>
   );
 }
