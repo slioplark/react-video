d1aba0bc7525d063e182b8b5ba391dfb6fe61e20
Mason <jeailfy9520@outlook.com>
2021-05-08 00:11:15 +0800

    feat: add log in in firebase

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,20 +3,20 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import {
   useToast,
   FormControl,
-  FormLabel,
   FormErrorMessage,
+  Link,
   Input,
   Button,
+  Divider,
 } from '@chakra-ui/react'
 import { LoginWrapper } from './style'
 import * as yup from 'yup'
-import db from '../../db'
+import * as auth from '../../db/auth'
 
 function Login() {
-  const auth = db.auth()
   const toast = useToast()
   const schema = yup.object().shape({
-    account: yup.string().required(),
+    email: yup.string().required(),
     password: yup.string().required().min(8),
   })
 
@@ -30,10 +30,14 @@ function Login() {
 
   const onSubmit = async (values) => {
     try {
-      const user = await auth.signInWithEmailAndPassword(
-        values.account,
-        values.password
-      )
+      const { email, password } = values
+      const user = await auth.signInWithEmailAndPassword(email, password)
+      if (!user.emailVerified) throw new Error('Please verify your email')
+      toast({
+        title: 'Log in created.',
+        status: 'success',
+        isClosable: true,
+      })
     } catch (err) {
       toast({
         title: err.message,
@@ -46,9 +50,9 @@ function Login() {
   return (
     <LoginWrapper>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <FormControl className="form-controller" isInvalid={errors.account}>
-          <Input placeholder="Email" {...register('account')} />
-          <FormErrorMessage>{errors.account?.message}</FormErrorMessage>
+        <FormControl className="form-controller" isInvalid={errors.email}>
+          <Input placeholder="Email" {...register('email')} />
+          <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
         </FormControl>
         <FormControl className="form-controller" isInvalid={errors.password}>
           <Input
@@ -60,7 +64,7 @@ function Login() {
           <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
         </FormControl>
         <Button w="100%" type="submit" colorScheme="teal">
-          Submit
+          Log In
         </Button>
       </form>
       <Link color="teal.500" href="#">
