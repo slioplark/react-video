85da5f8e085660c75c905f8014d9886f316177d0
Mason <jeailfy9520@outlook.com>
2021-05-07 23:02:32 +0800

    feat: add sign up component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import store from './store';
+import store from './store'
 
-import Header from './common/header';
-import Home from './pages/home';
-import Love from './pages/love';
-import Play from './pages/play';
-import Login from './pages/login';
+import Header from './common/header'
+import Home from './pages/home'
+import Love from './pages/love'
+import Play from './pages/play'
+import Login from './pages/login'
+import SignUp from './pages/sign-up'
 
 function App() {
   return (
@@ -18,9 +19,10 @@ function App() {
         <Route path="/react-video/love" exact component={Love} />
         <Route path="/react-video/play/:id" exact component={Play} />
         <Route path="/react-video/login" exact component={Login} />
+        <Route path="/react-video/signUp" exact component={SignUp} />
       </BrowserRouter>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
